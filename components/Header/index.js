2fd1f0347cb53e59c09df6a4825ec9c8b6eadc7a
Burak Tokak <iletisim@buraktokak.com>
2023-07-29 11:40:46 +0300

    header menu responseive system

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from "./style.module.css"
 
 function Header(){
+
+  let [menuState, setMenuState] = useState(false);
 
   return(
     <>
@@ -12,14 +15,14 @@ function Header(){
 
           <ul className={styles.menuMobile} style={{display: "auto"}}>
             <li>
-              <div onClick={() => setMobileSearchState("open")}>
-                <svg style={{position: "relative", top: 5, marginLeft: 5, marginRight: 5}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="21" height="21" fill="#333">
-                  <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/>
+              <div onClick={() => setMenuState(!menuState)}>
+                <svg style={{position: "relative", top: 5, marginLeft: 5, marginRight: 5}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="#333">
+                  <path d="M4 6H20M4 12H20M4 18H20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                 </svg>
               </div>
             </li>
           </ul>
-          <ul className={styles.menu}>
+          <ul className={styles.menu} style={menuState ? {display: "block"} : {}}>
             <li><Link href="/">Tools</Link></li>
             <li><Link href="/license/">License</Link></li>
             <li><Link href="/contact/">Contact</Link></li>
